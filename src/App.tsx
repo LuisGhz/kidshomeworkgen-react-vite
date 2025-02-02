76678ee60bade3ad282596024ab15cd7f602fccc
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-02 12:31:17 -0600

    Set basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Tabs } from "antd";
+import type { TabsProps } from "antd";
 import "./App.css";
+import { ElementaryMathsTab } from "./components/ElementaryMathsTab";
+
+const items: TabsProps["items"] = [
+  {
+    key: "1",
+    label: "Matematicas",
+    children: <ElementaryMathsTab />,
+  },
+];
 
 function App() {
-  return <></>;
+  return (
+    <main className="App">
+      <Tabs items={items} />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/ElementaryMathsTab.tsx
@@ -0,0 +1,18 @@
+import { Divider } from "antd";
+
+export const ElementaryMathsTab = () => {
+  return (
+    <div>
+      <h1>Matematicas primaria</h1>
+      <Divider />
+      <h2>Sumas</h2>
+      <Divider />
+      <h2>Restas</h2>
+      <Divider />
+      <h2>Multiplicaciones</h2>
+      <Divider />
+      <h2>Divisiones</h2>
+      <Divider />
+    </div>
+  );
+};
