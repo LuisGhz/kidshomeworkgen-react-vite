43025a90396bfd166c0a7151ad7b9fb262d59371
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-14 15:46:17 -0600

    Create divisions and multiplications form

---
+++ src/components/elementary/DivisionsForm.tsx
@@ -0,0 +1,54 @@
+import { Form, InputNumber, Button, Checkbox } from "antd";
+import { getPdfFile } from "../../services/PdfFile.service";
+import type { Divisions } from "../../types/ElementaryMathsForm.type";
+import { buildQueryString } from "../../utils/buildQueryString.util";
+
+export const DivisionsForm = () => {
+  const onFinish = async (values: Divisions) => {
+    const query = buildQueryString("/basic-maths/divisions", values);
+    await getPdfFile(query);
+  };
+
+  return (
+    <>
+      <Form
+        name="elementary-divisions"
+        initialValues={{
+          pages: 1,
+          dividendDigits: 5,
+          divisorDigits: 2,
+          canHaveRemainder: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item<Divisions> label="Páginas" name="pages">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item<Divisions>
+          label="Digitos para el dividendo"
+          name="dividendDigits"
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item<Divisions>
+          label="Digitos para el divisor"
+          name="divisorDigits"
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item<Divisions>
+          label="Incluir decimales"
+          name="canHaveRemainder"
+          valuePropName="checked"
+        >
+          <Checkbox />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Generar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
---
+++ src/components/elementary/MultiplicationsForm.tsx
@@ -0,0 +1,46 @@
+import { Form, InputNumber, Button } from "antd";
+import { getPdfFile } from "../../services/PdfFile.service";
+import type { Multiplications } from "../../types/ElementaryMathsForm.type";
+import { buildQueryString } from "../../utils/buildQueryString.util";
+
+export const MultiplicationsForm = () => {
+  const onFinish = async (values: Multiplications) => {
+    const query = buildQueryString("/basic-maths/multiplications", values);
+    await getPdfFile(query);
+  };
+
+  return (
+    <>
+      <Form
+        name="elementary-multiplications"
+        initialValues={{
+          pages: 1,
+          multiplicandDitis: 5,
+          multiplierDigits: 5,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item<Multiplications> label="Páginas" name="pages">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item<Multiplications>
+          label="Digitos para el multiplicando"
+          name="multiplicandDitis"
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item<Multiplications>
+          label="Digitos para el multiplicador"
+          name="multiplierDigits"
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Generar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
