4c470640f51d19898ffacbe7c8ea7ad98d0b0b73
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-14 15:29:02 -0600

    Create elementary maths tab

--- src/components/ElementaryMathsTab.tsx
+++ src/components/ElementaryMathsTab.tsx
@@ -1,17 +1,39 @@
 import { Divider } from "antd";
+import { AdditionsForm } from "./elementary/AdditionsForm";
 
 export const ElementaryMathsTab = () => {
   return (
     <div>
       <h1>Matematicas primaria</h1>
       <Divider />
-      <h2>Sumas</h2>
+      <h2>Operaciones básicas</h2>
       <Divider />
-      <h2>Restas</h2>
+      <h3>Sumas</h3>
+      <AdditionsForm />
       <Divider />
-      <h2>Multiplicaciones</h2>
+      <h3>Restas</h3>
+      {/* 
+        pages int
+        minNumber int
+        maxNumber int
+        isWithNegative boolean
+        isWithDecimal boolean1
+       */}
       <Divider />
-      <h2>Divisiones</h2>
+      <h3>Multiplicaciones</h3>
+      {/* 
+        pages int
+        minFactorValue int
+        maxFactorValue int
+        isWithDecimal boolean
+      */}
+      <Divider />
+      <h3>Divisiones</h3>
+      {/* 
+        pages int
+        minNumber int
+        isWithDecimal boolean
+       */}
       <Divider />
     </div>
   );
