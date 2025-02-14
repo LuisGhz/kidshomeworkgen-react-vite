076647b67d63e1f452545c4b33874f5d7a58bee8
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-14 15:38:47 -0600

    Add missing ops

--- src/services/PdfFile.service.ts
+++ src/services/PdfFile.service.ts
@@ -1,8 +1,9 @@
 import { getFile } from "../api";
+import { RequestInitFile } from "../api/models/RequestInitFile.model";
 
-export const getPdfFile = async (path: string) => {
+export const getPdfFile = async (path: string, ops: RequestInitFile = {}) => {
   try {
-    await getFile(path);
+    await getFile(path, ops);
   } catch {
     console.error("Error al descargar el archivo");
   }
