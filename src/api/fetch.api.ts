a27a24b3c1ac854013f433e65d691410e8edb013
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-14 15:38:35 -0600

    Implement new model and open file in browser

--- src/api/fetch.api.ts
+++ src/api/fetch.api.ts
@@ -1,10 +1,11 @@
 import { getFileNameFromContentDisposition } from "../utils/getFileNameFromContentDisposition.util";
 import { downloadFile } from "../utils/downloadFile.util";
 import { openFile } from "../utils/openFile.util";
+import { RequestInitFile } from "./models/RequestInitFile.model";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const getFile = async (path: string, ops: RequestInit = {}, isDownload: boolean = true) => {
+export const getFile = async (path: string, ops: RequestInitFile = {}) => {
   console.log(`${API_URL}${path}`);
   const response = await fetch(`${API_URL}${path}`, {
     method: "GET",
@@ -16,8 +17,6 @@ export const getFile = async (path: string, ops: RequestInit = {}, isDownload: b
 
   const filename = getFileNameFromContentDisposition(response);
   const blob = await response.blob();
-  if (isDownload)
-    downloadFile(filename, blob);
-  else
-    openFile(blob);
+  if (ops.isForDownload) downloadFile(filename, blob);
+  else openFile(blob);
 };
