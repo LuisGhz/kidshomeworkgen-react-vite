e68dd5a90875c5ec59af85a69b16a43dec2180fc
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-14 15:56:47 -0600

    Implement collapse

--- src/components/ElementaryMathsTab.tsx
+++ src/components/ElementaryMathsTab.tsx
@@ -1,28 +1,50 @@
-import { Divider } from "antd";
+import { Collapse, CollapseProps, Divider } from "antd";
 import { AdditionsForm } from "./elementary/AdditionsForm";
 import { SubstractionsForm } from "./elementary/SubstractionsFrom";
 import { MultiplicationsForm } from "./elementary/MultiplicationsForm";
 import { DivisionsForm } from "./elementary/DivisionsForm";
 
+const basicMathsItems: CollapseProps["items"] = [
+  {
+    key: "1",
+    label: "Sumas",
+    children: <AdditionsForm />,
+  },
+  {
+    key: "2",
+    label: "Restas",
+    children: <SubstractionsForm />,
+  },
+  {
+    key: "3",
+    label: "Multiplicaciones",
+    children: <MultiplicationsForm />,
+  },
+  {
+    key: "4",
+    label: "Divisiones",
+    children: <DivisionsForm />,
+  },
+];
+
+const mathsItems: CollapseProps["items"] = [
+  {
+    key: "1",
+    label: "Operaciones básicas",
+    children: (
+      <>
+        <Collapse items={basicMathsItems} />
+      </>
+    ),
+  },
+];
+
 export const ElementaryMathsTab = () => {
   return (
     <div>
-      <h1>Matematicas primaria</h1>
+      <h1>Matematicas</h1>
       <Divider />
-      <h2>Operaciones básicas</h2>
-      <Divider />
-      <h3>Sumas</h3>
-      <AdditionsForm />
-      <Divider />
-      <h3>Restas</h3>
-      <SubstractionsForm />
-      <Divider />
-      <h3>Multiplicaciones</h3>
-      <MultiplicationsForm />
-      <Divider />
-      <h3>Divisiones</h3>
-      <DivisionsForm />
-      <Divider />
+      <Collapse items={mathsItems} />
     </div>
   );
 };
